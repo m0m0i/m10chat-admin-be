3d374ab06ad27dd808a0abf3f1fbb01cc5ee46d4
m0m0i <hirocorp01@gmail.com>
2022-01-20 16:16:39 +0900

    add error handler

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import { ErrorWithStatus } from "../types";
 
 const app = express();
 const port = process.env.PORT ? process.env.PORT : 3000;
+
+function error(status: number, msg: string) {
+  const err = new Error(msg) as ErrorWithStatus;
+  err.status = status;
+  return err;
+}
 
 app.get('/', (req, res) => {
   res.send('woof!')
